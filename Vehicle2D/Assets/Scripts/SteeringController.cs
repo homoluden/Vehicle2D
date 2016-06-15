224281b9a611c3aae167af84347603944a41561e
Alexey Matveev <alexey_matveev@epam.com>
2016-06-16 00:25:27 +0300

    Initial implementation of 2D car

---
+++ Vehicle2D/Assets/Scripts/SteeringController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringController : MonoBehaviour
+{
+    private Rigidbody2D _carBody;
+    private float _minForce = 0.0f;
+
+    public float Force = 0.5f;
+    public float MaxAngle = 60.0f;
+    public Rigidbody2D LeftWheel;
+    public Rigidbody2D RightWheel;
+    
+	void Start ()
+	{
+	    _carBody = GetComponent<Rigidbody2D>();
+        _minForce = Force * 0.1f;
+        MaxAngle = Mathf.Abs(MaxAngle);
+	}
+	
+	void Update ()
+    {
+        float angleLeft = Quaternion.Angle(_carBody.transform.rotation, LeftWheel.transform.rotation);
+	    
+	    float angleRight = Quaternion.Angle(_carBody.transform.rotation, RightWheel.transform.rotation);
+	    
+        var angleAvg = Mathf.Abs((angleLeft + angleRight) * 0.5f);
+        
+        var force = LerpClamp(_minForce, Force, (Mathf.Abs(angleAvg - MaxAngle)) / MaxAngle);
+
+        //Debug.Log(string.Format("Left: {0}   ||   Right: {1}", angleLeft, angleRight));
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+	    {
+            // Turning Left
+            LeftWheel.AddTorque(force * LeftWheel.mass, ForceMode2D.Impulse);
+            RightWheel.AddTorque(force * RightWheel.mass, ForceMode2D.Impulse);
+            _carBody.AddTorque(-force * (LeftWheel.mass + RightWheel.mass) * 0.5f, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            // Turning Right
+            LeftWheel.AddTorque(-force * LeftWheel.mass, ForceMode2D.Impulse);
+            RightWheel.AddTorque(-force * RightWheel.mass, ForceMode2D.Impulse);
+            _carBody.AddTorque(force * (LeftWheel.mass + RightWheel.mass) * 0.5f, ForceMode2D.Impulse);
+        }
+    }
+    
+    private float LerpClamp(float min, float max, float t)
+    {
+        if (min >= max || t >= 1.0f)
+        {
+            return max;
+        }
+
+        if (t <= 0.0f)
+        {
+            return min;
+        }
+
+        var diff = max - min;
+
+        return min + diff * t;
+    }
+}
---
+++ Vehicle2D/Assets/Scripts/ThrottleController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrottleController : MonoBehaviour
+{
+    private Rigidbody2D _carBody;
+
+    public float Force = 5.0f;
+    public float RotationBalance = 0.1f;
+    public Rigidbody2D LeftWheel;
+    public Rigidbody2D RightWheel;
+
+    // Use this for initialization
+    void Start () {
+        _carBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            // Turning Left
+            LeftWheel.AddRelativeForce(Vector2.up * Force * LeftWheel.mass, ForceMode2D.Force);
+            RightWheel.AddRelativeForce(Vector2.up * Force * RightWheel.mass, ForceMode2D.Force);
+
+            _carBody.AddTorque(-RotationBalance * (LeftWheel.mass + RightWheel.mass), ForceMode2D.Force);
+        }
+
+        if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+        {
+            // Turning Left
+            LeftWheel.AddRelativeForce(Vector2.down * Force * LeftWheel.mass, ForceMode2D.Force);
+            RightWheel.AddRelativeForce(Vector2.down * Force * RightWheel.mass, ForceMode2D.Force);
+
+            _carBody.AddTorque(RotationBalance * (LeftWheel.mass + RightWheel.mass), ForceMode2D.Force);
+        }
+    }
+}
