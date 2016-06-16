04ce690ca76eb15aec63575a781ede49d5d40d39
Alexey Matveev <alexey_matveev@epam.com>
2016-06-17 00:44:53 +0300

    Implement steering friction to make the vehicle able to turn.

---
+++ Vehicle2D/Assets/Scripts/SteeringFriction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringFriction : MonoBehaviour
+{
+    private Rigidbody2D _body;
+
+    public float FrictionFactor = 0.01f;
+
+	void Start () {
+        _body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var locVel = transform.InverseTransformDirection(_body.velocity);
+        var dx = -locVel.x;
+
+        _body.AddForce(FrictionFactor * dx * _body.mass * Vector2.right, ForceMode2D.Force);
+    }
+}
