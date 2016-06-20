c891c0f0f700dc74a77a371b1a94c1e890687478
Alexey Matveev <alexey_matveev@epam.com>
2016-06-21 01:57:36 +0300

    Rework tires model and steering

--- Vehicle2D/Assets/Scripts/ThrottleController.cs
+++ Vehicle2D/Assets/Scripts/ThrottleController.cs
@@ -6,7 +6,6 @@ public class ThrottleController : MonoBehaviour
     private Rigidbody2D _carBody;
 
     public float Force = 5.0f;
-    public float RotationBalance = 0.1f;
     public Rigidbody2D LeftWheel;
     public Rigidbody2D RightWheel;
 
@@ -19,22 +18,20 @@ public class ThrottleController : MonoBehaviour
 	void Update () {
         if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
-            var force = Vector2.up * Force * LeftWheel.mass;
-            //LeftWheel.AddRelativeForce(, ForceMode2D.Force);
-            //RightWheel.AddRelativeForce(Vector2.up * Force * RightWheel.mass, ForceMode2D.Force);
+            var leftForce = LeftWheel.transform.up * Force;
+            var rightForce = RightWheel.transform.up * Force;
 
-            _carBody.AddForce(force, ForceMode2D.Impulse);
-            //_carBody.AddTorque(-RotationBalance * (LeftWheel.mass + RightWheel.mass), ForceMode2D.Force);
+            LeftWheel.AddForce(leftForce, ForceMode2D.Impulse);
+            RightWheel.AddForce(rightForce, ForceMode2D.Impulse);
         }
 
         if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
         {
-            var force = Vector2.down * Force * LeftWheel.mass;
-            //LeftWheel.AddRelativeForce(Vector2.down * Force * LeftWheel.mass, ForceMode2D.Force);
-            //RightWheel.AddRelativeForce(Vector2.down * Force * RightWheel.mass, ForceMode2D.Force);
+            var leftForce = -LeftWheel.transform.up * Force;
+            var rightForce = -RightWheel.transform.up * Force;
 
-            _carBody.AddForce(force, ForceMode2D.Impulse);
-            //_carBody.AddTorque(RotationBalance * (LeftWheel.mass + RightWheel.mass), ForceMode2D.Force);
+            LeftWheel.AddForce(leftForce, ForceMode2D.Impulse);
+            RightWheel.AddForce(rightForce, ForceMode2D.Impulse);
         }
     }
 }
---
+++ Vehicle2D/Assets/Scripts/Tire.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tire : MonoBehaviour {
+
+    private Rigidbody2D _body;
+
+    public float Friction = 600.0f;
+
+	// Use this for initialization
+	void Start () {
+        _body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var lateralDir = _body.transform.right;
+        var lateralVel = Vector3.Project(_body.velocity, lateralDir);
+        _body.AddForce(-lateralVel * _body.mass * Friction);
+	}
+}
